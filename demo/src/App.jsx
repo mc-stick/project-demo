16b9ccf222c7382ddde1a6a9943110d2eb5e11fb
mc-stick <>
2025-11-26 10:22:39 -0400

    cmm 4

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -8,12 +8,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-       
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
